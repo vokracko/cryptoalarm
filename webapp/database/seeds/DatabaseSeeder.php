282d7195a03567bd36c59a39615ec08e7e06f836
Lukáš Vokráčko <lukas.vokracko@gmail.com>
2018-05-21 11:16:21 +0200

    remove notification seeder

--- webapp/database/seeds/DatabaseSeeder.php
+++ webapp/database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
             AddressesTableSeeder::class,
             WatchlistsTableSeeder::class,
             SettingsTableSeeder::class,
-            NotificationsTableSeeder::class,
             IdentitiesTableSeeder::class,
         ]);
     }
